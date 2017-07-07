8fc9209e795123996be9779621d0c601ba7e758a
Li Shuzu <will@Wills-MacBook-Pro.local>
2017-07-07 13:04:55 +1200

    swift version of totoro talk

---
+++ totorotalk2/TotoroTalk/TotoroTalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TotoroTalk
+//
+//  Created by Li Shuzu on 7/07/17.
+//  Copyright © 2017 TimePic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
